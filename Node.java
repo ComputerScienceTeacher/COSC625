66f6745efae78d7c2b7bf9d498a5fa7e0676f1d6
ComputerScienceTeacher <62949050+ComputerScienceTeacher@users.noreply.github.com>
2021-09-28 19:09:09 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,30 +1,28 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
-
-    
-    
 public class Node{
-private String element;    
+  private String element;    
     
-Node next;    
-SLLvideo rentVideo = new SLLvideo();    
-    
-public Node(String element, Node next)
-{    
-    
-this.element = element;
-this.next = next;}
+    Node next;    
+    SLLvideo rentVideo = new SLLvideo();    
 
-public String getElement(){return element;
-}
-public Node getNext(){return next;}
-public void setElement(String element){this.element = element;}
-public void setNext(Node next){this.next = next;}
+  public Node(String element, Node next){    
+    this.element = element;
+    this.next = next;
+  }
+
+  public String getElement(){
+    return element;
+  }
+
+  public Node getNext(){
+    return next;
+  }
+
+  public void setElement(String element){
+    this.element = element;
+  }
+
+  public void setNext(Node next){
+    this.next = next;
+  }
 
 }
